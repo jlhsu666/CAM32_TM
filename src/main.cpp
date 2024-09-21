8b4c4ce58683c79f1f2ed71e3905cc7b72bad15c
Ron <srl@gapps.knu.edu.tw>
2024-09-21 10:34:48 +0800

    Config OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,189 @@
+#include <WiFi.h>
+#include <esp32-hal-ledc.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include "esp_http_server.h"
+#include "img_converters.h"
+
+const char* ssid = "Ron";
+const char* password = "0963083131";
+
+const char* apssid = "ESP32-Ron";
+const char* appassword = "12345678";
+
+String Feedback="";
+String Command="";
+String cmd="";
+
+typedef struct {
+    httpd_req_t *req;
+    size_t len;
+} jpg_chunking_t;
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY= "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART ="Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+httpd_handle_t stream_httpd = NULL;
+httpd_handle_t camera_httpd = NULL;
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27  
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+void initCamera()
+{
+ camera_config_t config;
+ config.ledc_channel = LEDC_CHANNEL_0;
+ config.ledc_timer = LEDC_TIMER_0;
+ config.pin_d0 = Y2_GPIO_NUM;
+ config.pin_d1 = Y3_GPIO_NUM;
+ config.pin_d2 = Y4_GPIO_NUM;
+ config.pin_d3 = Y5_GPIO_NUM;
+ config.pin_d4 = Y6_GPIO_NUM;
+ config.pin_d5 = Y7_GPIO_NUM;
+ config.pin_d6 = Y8_GPIO_NUM;
+ config.pin_d7 = Y9_GPIO_NUM;
+ config.pin_xclk = XCLK_GPIO_NUM;
+ config.pin_pclk = PCLK_GPIO_NUM;
+ config.pin_vsync = VSYNC_GPIO_NUM;
+ config.pin_href = HREF_GPIO_NUM;
+ config.pin_sscb_sda = SIOD_GPIO_NUM;
+ config.pin_sscb_scl = SIOC_GPIO_NUM;
+ config.pin_pwdn = PWDN_GPIO_NUM;
+ config.pin_reset = RESET_GPIO_NUM;
+ config.xclk_freq_hz = 20000000;
+ config.pixel_format = PIXFORMAT_JPEG; 
+  
+ if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+ } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+ }
+  
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void startCameraServer()
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    httpd_uri_t index_uri ={
+        .uri = "/",
+        .method = HTTP_GET,
+        .handler = index_handler,
+        .user_ctx = NULL
+    };
+
+    httpd_uri_t status_uri ={
+        .uri = "/status",
+        .method = HTTP_GET,
+        .handler = status_handler,
+        .user_ctx = NULL
+    };
+
+    httpd_uri_t cmd_uri ={
+        .uri = "/control",
+        .method = HTTP_GET,
+        .handler = cmd_handler,
+        .user_ctx = NULL
+    };
+
+    httpd_uri_t capture_uri ={
+        .uri = "/capture",
+        .method = HTTP_GET,
+        .handler = capture_handler,
+        .user_ctx = NULL
+    };
+
+    httpd_uri_t stream_uri ={
+        .uri = "/stream",
+        .method = HTTP_GET,
+        .handler = stream_handler,
+        .user_ctx = NULL
+    };
+
+    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+    if (httpd_start(&camera_httpd, &config)==ESP_OK){
+        httpd_register_uri_handler(camera_httpd, &index_uri);
+        httpd_register_uri_handler(camera_httpd, &cmd_uri);
+        httpd_register_uri_handler(camera_httpd, &status_uri);
+        httpd_register_uri_handler(camera_httpd, &capture_uri);
+    }
+
+    config.server_port +=1;
+    config.ctrl_port +=1;
+    Serial.printf("Starting stream server on port : '%d'\n", config.server_port);
+    if (httpd_start(&camera_httpd, &config)==ESP_OK){
+        httpd_register_uri_handler(camera_httpd, &stream_uri);
+    }
+
+}
+
 void setup()
 {
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+    Serial.begin(115200);
+    Serial.println();
+    initCamera();
+
+    ledcAttachPin(4, 4);
+    ledcSetup(4,5000,8);
+
+    WiFi.mode(WIFI_AP_STA);
+
+    for (int i=0;i<2;i++){
+        WiFi.begin(ssid,password);
+        delay(1000);
+        Serial.println("");
+        Serial.print("Connecting to ");
+        Serial.println(ssid);
+
+        long int StartTime=millis();
+        while (WiFi.status() != WL_CONNECTED){
+            delay(500);
+            if((StartTime+5000) < millis()) break;
+        }
+
+        if(WiFi.status() == WL_CONNECTED){
+            WiFi.softAP(WiFi.localIP().toString()+"_"+(String)apssid.c_str(),appassword);
+            Serial.println("");
+            Serial.println("STAIP address: ");
+            Serial.println(WiFi.localIP());
+            Serial.println("");
+        }
+    }
+
+    Serial.println("");
+    Serial.println("APIP address: ");
+    Serial.println(WiFi.softAPIP());
+    Serial.println("");
+
+    startCameraServer();
 
 }
 
